c33f02f6cf281b9df48b01aa7aa8390441335a7a
Kan muru <gautiergueri@gmail.com>
2026-05-17 17:48:54 +0200

    Initial dotfiles

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DOTFILES=$(dirname "$(realpath "$0")")
+
+mkdir -p ~/.config/hypr
+mkdir -p ~/.config/omarchy/user/hypr
+
+ln -sf $DOTFILES/hypr/input.conf ~/.config/hypr/input.conf
+ln -sf $DOTFILES/hypr/bindings.conf ~/.config/hypr/bindings.conf
+ln -sf $DOTFILES/omarchy/user/hypr/config.conf ~/.config/omarchy/user/hypr/config.conf
+
+echo "✅ Dotfiles installés !"
